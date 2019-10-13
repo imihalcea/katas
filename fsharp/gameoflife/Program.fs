b44ffa8c026135c37d2252b17668cbe65f693800
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2019-10-13 11:49:57 +0200

    initial commit; MonoGame works with Fsharp and .Net Core

---
+++ fsharp/gameoflife/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open GameOfLife
+
+[<EntryPoint>]
+let main argv =
+    use g = new GameUI()
+    g.Run()
+    0
---
+++ fsharp/gameoflife/UserInterface.fs
@@ -0,0 +1,37 @@
+module GameOfLife
+ 
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open System
+open Microsoft.Xna.Framework.Input
+ 
+type GameUI () as x =
+    inherit Game()
+ 
+    do x.Content.RootDirectory <- "Content"
+    let graphics = new GraphicsDeviceManager(x)
+    do graphics.SynchronizeWithVerticalRetrace <- true
+    do x.TargetElapsedTime <- TimeSpan.FromSeconds(0.15)
+    do x.IsFixedTimeStep <- true
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+ 
+    override x.Initialize() =
+        do spriteBatch <- new SpriteBatch(x.GraphicsDevice)
+        do graphics.PreferredBackBufferWidth <- 1920;
+        do graphics.PreferredBackBufferHeight <- 1080;
+        do graphics.IsFullScreen <- true;
+        do graphics.ApplyChanges();
+        do base.Initialize()
+        ()
+    
+    override x.LoadContent() =
+        ()
+ 
+    override x.Update (gameTime) =
+        if GamePad.GetState(PlayerIndex.One).Buttons.Back = ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape) then
+            x.Exit()
+        ()
+ 
+    override x.Draw (gameTime) =
+        do x.GraphicsDevice.Clear Color.CornflowerBlue
+        ()
