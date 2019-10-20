b79ffab84e283224fd4ce160afc990b040c771f1
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2019-10-20 19:07:28 +0200

    glider

--- fsharp/gameoflife/src/Program.fs
+++ fsharp/gameoflife/src/Program.fs
@@ -5,8 +5,9 @@ open Domain
 
 [<EntryPoint>]
 let main argv =
-    let blinker = [Live(50,50);Live(50,51);Live(50,52)]
-    let grid = GoL.init blinker
+    let blinker = [Live(25,25);Live(25,26);Live(25,27)]
+    let glider = [Live(7,6);Live(8,7);Live(8,8);Live(7,8);Live(6,8)]
+    let grid = GoL.init glider
     use g = new GameUI(5, grid, Grid.evolve)
     g.Run()
     0
